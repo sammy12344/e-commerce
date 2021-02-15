b49056eb1433961b006e68b98e3c3e18df417902
sammy12344 <sammydd1124@gmail.com>
2021-02-15 13:21:52 +0100

    several changes on styles and components

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shoppage/shop.component';
+import Header from './components/header/header.component';
 
 function App(props) {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
